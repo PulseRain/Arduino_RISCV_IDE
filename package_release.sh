e66febdff7914fc15f8f948326064c8620592b59
PulseRainmaker <pulserain@pulserain.com>
2020-05-19 21:48:57 -0700

    add support for Nuand bladeRF

--- package_release.sh
+++ package_release.sh
@@ -18,8 +18,8 @@
 #
 ###############################################################################
 
-Reindeer_version=1.3.3
-Rattlesnake_version=1.3.3
+Reindeer_version=1.3.4
+Rattlesnake_version=1.3.4
 
 Reindeer_compiler_version=1.0.0
 Reindeer_upload_version=2.1.0
@@ -163,7 +163,8 @@ echo '          },'
 echo '          "boards": ['
 echo '            {"name": "Gnarly Grey UPDuinoV2 Board (Lattice UP5K)"},'
 echo '            {"name": "STEP CYC10 Board (Intel 10CL016YU256C8G)"},'
-echo '            {"name": "Efinix Trion T20 BGA256 Development Kit"}'
+echo '            {"name": "Efinix Trion T20 BGA256 Development Kit"}',
+echo '            {"name": "Nuand bladeRF"}'
 echo '          ],'
 echo '          "toolsDependencies": ['
 echo '            {'
